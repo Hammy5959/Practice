15ee1621d9dab916e76985ff6105a5a4e38a9620
hamid <ziddisheikh60@gmail.com>
2024-01-18 05:36:56 +0500

    calculator project

--- Calculator Project/calculator.ts
+++ Calculator Project/calculator.ts
@@ -1,54 +1,70 @@
 import inquirer from "inquirer";
+import chalk from "chalk"
+import chalkAnimation from "chalk-animation"
 
-function main() {
-  inquirer
+const sleep=()=>{
+  return new Promise((res)=>{
+    setTimeout(res,2000)
+  })
+}
+async function welcome(){
+  let rainbowTitle= chalkAnimation.karaoke(`--Lets start Calculation--`)
+  await sleep()
+  rainbowTitle.stop()
+}
+welcome()
+async function calculator() {
+  await inquirer
     .prompt([
-      { type: "input", name: "num1", meassage: "Enter first number" },
-      { type: "input", name: "num2", meassage: "Enter second number" },
       {
         type: "list",
         name: "operator",
-        choices: ["+", "-", "*", "/"],
-        meassage: "Enter your operator",
+        message: "Enter your operator",
+        choices: ["Addition", "Substract", "Multiplication", "Division"],
+      },
+      {
+        type: "number",
+        name: "num1",
+        message: "Enter your first number",
+      },
+      {
+        type: "number",
+        name: "num2",
+        message: "Enter your second number",
       },
     ])
     .then((answers) => {
-      console.log(answers);
-      /*if (operator == "+") {
-        sum(num1, num2);
-      } else if (operator == "-") {
-        substract(num1, num2);
-      } else if (operator == "*") {
-        multiplication(num1, num2);
-      } else if (operator == "/") {
-        division(num1, num2);
-      } else {
-        console.log("Invalid Operator");
-      }*/
-      // Use user feedback for... whatever!!
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else went wrong
+      //console.log(answers)
+      if (answers.operator == "Addition") {
+        console.log(
+          `${answers.num1} + ${answers.num2} = ${answers.num1 + answers.num2}`
+        );
+      } else if (answers.operator == "Substract") {
+        console.log(
+          `${answers.num1} - ${answers.num2} = ${answers.num1 - answers.num2}`
+        );
+      } else if (answers.operator == "Multiplication") {
+        console.log(
+          `${answers.num1} * ${answers.num2} = ${answers.num1 * answers.num2}`
+        );
+      } else if (answers.operator == "Division") {
+        console.log(
+          `${answers.num1} / ${answers.num2} = ${answers.num1 / answers.num2}`
+        );
       }
     });
 }
-function sum(num1: number, num2: number) {
-  const result = num1 + num2;
-  console.log("Sum", result);
+calculator()
+
+async function startAgain(){
+  do{
+    await calculator()
+    var again = await inquirer
+    .prompt({
+      type:"input",
+      name:"restart",
+      message:"Do you want to continue? press y or n"
+    })
+  }while(again.restart=="Y"||again.restart=="y"||again.restart=="yes"||again.restart=="YES")
 }
-function substract(num1: number, num2: number) {
-  const result = num1 - num2;
-  console.log("Substraction", result);
-}
-function multiplication(num1: number, num2: number) {
-  const result = num1 * num2;
-  console.log("Multiply", result);
-}
-function division(num1: number, num2: number) {
-  const result = num1 / num2;
-  console.log("Divide", result);
-}
-main();
+startAgain()
